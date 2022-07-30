5c7f6c8f57ce45044b9e1daf0dd395480623bbad
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-30 20:18:00 +0530

    tailwind and images

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+  // why we are using index.js and writing exports here in components is that later we need to add multiple import statements for different components so that will add a huge import over there so to avoid that we are importing everything as an object from components folder so that index.js will handel all those imports.
+  
+  export { default as Header } from './Header'; 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 
+import { BrowserRouter as Router, } from "react-router-dom";
+
+
 import App from './App';
+
+ReactDOM.render( //router should always be on top of app component
+    <Router>         
+        <App />
+    </Router>, document.getElementById('root')
+);
