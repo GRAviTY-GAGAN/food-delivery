6590c255a1131e9011a382c9ec2806eddc44aa16
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-31 12:53:53 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import { Header } from './components';
+import { AnimatePresence } from 'framer-motion';
+import { CreateContainer, MainContainer } from './components';
 
 
 const App = () => {
   return (
-    <div className='w-screen h-auto flex flex-col'>
+
+<AnimatePresence> {/* By wraping everything inside AnimatePresense it makes this whole component have the animations and we need not import this on every component so we are adding it here in the top level component */}
+    <div className='w-screen h-auto flex flex-col bg-primary'>
       <Header />
+
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+            <Route path='/*' element={<MainContainer />}/>
+            <Route path='/createItem' element={<CreateContainer />}/>
+        </Routes>
+      </main>
     </div>
+</AnimatePresence>
+    
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
   // why we are using index.js and writing exports here in components is that later we need to add multiple import statements for different components so that will add a huge import over there so to avoid that we are importing everything as an object from components folder so that index.js will handel all those imports.
   
   export { default as Header } from './Header'; 
+  export { default as MainContainer } from './MainContainer';
+  export { default as CreateContainer } from './CreateContainer';
