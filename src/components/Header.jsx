1eaad4c7925441b5f9ba598310d3d08e4b58c44d
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-08-01 19:35:50 +0530

    Main & Home container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,23 +30,21 @@ const Header = () => {
     localStorage.setItem('user', JSON.stringify(providerData[0]));  //setting the user data to localStorage so that when we refresh we still have the user data in localStorage.
     } else {
       setIsMenu(!isMenu)
-      localStorage.clear()
-
-      dispatch(
-        {
-          type : actionType.SET_USER,
-          user : null
-        }
-      )
     }
   };
 
   const logOut = ()  => {
-    setIsMenu(false)
-  }
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type : actionType.SET_USER,
+      user : null,
+    });
+  };
 
   return (
-    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
+    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary'>
         {/* desktop & tablet */}
         <div className='hidden md:flex w-full h-full items-center justify-between'>
             <Link to={'/'} className="flex items-center gap-2">
@@ -78,7 +76,7 @@ const Header = () => {
                 whileTap={{scale : 0.6 }} 
                 src={user ? user.photoURL : Avatar} 
                 className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full' 
-                alt="userprofile" 
+                alt="" 
                 onClick={login}
               />
               { isMenu && (
@@ -89,7 +87,8 @@ const Header = () => {
                 className=' w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
                 {user && user.email === 'gaganbn1999@gmail.com' && (
                   <Link to={'/createItem'}>
-                    <p className=' px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' >New Item <MdAdd />
+                    <p className=' px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                    onClick={() => setIsMenu(false)} >New Item <MdAdd />
                     </p>
                   </Link>
                 )}
@@ -123,7 +122,7 @@ const Header = () => {
                 whileTap={{scale : 0.6 }} 
                 src={user ? user.photoURL : Avatar} 
                 className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full' 
-                alt="userprofile" 
+                alt="" 
                 onClick={login}
               />
               { isMenu && (
@@ -139,10 +138,14 @@ const Header = () => {
 
                 <ul  
                   className='flex flex-col'>
-                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Home</li>
-                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Menu</li>
-                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About Us</li>
-                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Services</li>
+                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                    onClick={() => setIsMenu(false)}>Home</li>
+                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                    onClick={() => setIsMenu(false)}>Menu</li>
+                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                    onClick={() => setIsMenu(false)}>About Us</li>
+                    <li className='text-base rounded-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                    onClick={() => setIsMenu(false)}>Services</li>
                 </ul>
 
                 <p className=' m-2 p-2 flex rounded-md shadow-md items-center justify-center gap-3 cursor-pointer bg-gray-200 hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base' 
---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+import I1 from '../Img/i1.png';
+import F1 from '../Img/f1.png';
+import C3 from '../Img/c3.png';
+import Fi1 from '../Img/fi1.png';
+
+
+export const heroData = [
+    {id : 1, name : 'Icecream', decp: 'Cholocate & vanila', price : '5.25', imgageSrc : I1},
+    {id : 2, name : 'Strawberries', decp: 'Fresh Straberries', price : '10.25', imgageSrc : F1},
+    {id : 3, name : 'Chicken Kebab', decp: 'Mixed Kebab Plate', price : '8.25', imgageSrc : C3},
+    {id : 4, name : 'Fish Kebab', decp: 'Mixed Fish Kebab', price : '5.25', imgageSrc : Fi1},
+]
