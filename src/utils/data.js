1db1948f8d3e2cf94f7a56de989bf212b3f2f92c
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-08-01 21:07:57 +0530

    create container & loader

--- src/utils/data.js
+++ src/utils/data.js
@@ -5,8 +5,71 @@ import Fi1 from '../Img/fi1.png';
 
 
 export const heroData = [
-    {id : 1, name : 'Icecream', decp: 'Cholocate & vanila', price : '5.25', imgageSrc : I1},
-    {id : 2, name : 'Strawberries', decp: 'Fresh Straberries', price : '10.25', imgageSrc : F1},
-    {id : 3, name : 'Chicken Kebab', decp: 'Mixed Kebab Plate', price : '8.25', imgageSrc : C3},
-    {id : 4, name : 'Fish Kebab', decp: 'Mixed Fish Kebab', price : '5.25', imgageSrc : Fi1},
-]
+    {
+        id : 1, 
+        name : 'Icecream', 
+        decp: 'Cholocate & vanila', 
+        price : '5.25', 
+        imgageSrc : I1
+    },
+    {
+        id : 2, 
+        name : 'Strawberries', 
+        decp: 'Fresh Straberries', 
+        price : '10.25', 
+        imgageSrc : F1
+    },
+    {
+        id : 3, 
+        name : 'Chicken Kebab', 
+        decp: 'Mixed Kebab Plate', 
+        price : '8.25', 
+        imgageSrc : C3
+    },
+    {
+        id : 4, 
+        name : 'Fish Kebab', 
+        decp: 'Mixed Fish Kebab', 
+        price : '5.25', 
+        imgageSrc : Fi1
+    },
+];
+
+export const categories = [
+    {
+      id: 1,
+      name: "Chicken",
+      urlParamName: "chicken",
+    },
+    {
+      id: 2,
+      name: "Curry",
+      urlParamName: "curry",
+    },
+    {
+      id: 3,
+      name: "Rice",
+      urlParamName: "rice",
+    },
+    {
+      id: 4,
+      name: "Fish",
+      urlParamName: "fish",
+    },
+    {
+      id: 5,
+      name: "Fruits",
+      urlParamName: "fruits",
+    },
+    {
+      id: 6,
+      name: "Icecreams",
+      urlParamName: "icecreams",
+    },
+  
+    {
+      id: 7,
+      name: "Soft Drinks",
+      urlParamName: "drinks",
+    },
+  ];
